83f863a14786d9f945524cddae107b663ddb1f42
Samyukthaa02 <141002483+Samyukthaa02@users.noreply.github.com>
2026-01-22 13:07:36 +0530

    Migrate javax.persistence imports to jakarta.persistence in App.java

--- src/main/java/br/com/example/davidarchanjo/model/domain/App.java
+++ src/main/java/br/com/example/davidarchanjo/model/domain/App.java
@@ -1,9 +1,9 @@
 package br.com.example.davidarchanjo.model.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.Builder;
 import lombok.Data;
